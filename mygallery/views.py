fe097c05595d133ceaac80db60794d09b1ef168d
antonymburia <antonymburia10@gmail.com>
2022-03-26 21:07:00 +0300

    create image views

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import User,Image,Category,Location
+from django.core.exceptions import DoesNotExist
+
 
 # Create your views here.
+
+def pics(request):
+    category = Category.get_categories()
+    pictures = Image.all_pics()
+    location_pics = Location.get_location()
+
+    return render(request,'images.html',{'pictures': pictures, 'category': category, 'location_pics':location_pics })
+
+
