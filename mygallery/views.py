2f06616649f6e165ae625d5916a38b1b52c5f72a
antonymburia <antonymburia10@gmail.com>
2022-03-26 21:08:20 +0300

    create search image view

--- mygallery/views.py
+++ mygallery/views.py
@@ -13,4 +13,23 @@ def pics(request):
 
     return render(request,'images.html',{'pictures': pictures, 'category': category, 'location_pics':location_pics })
 
+def single_pic(request,id):
+    try:
+        pic = Image.objects.get(id = id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"single_image.html", {"pic":pic})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get('image')
+        searched_pics = Image.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request,'search.html',{"message":message,"image":searched_pics})
+
+    else:
+        message = "field cannot be empty"
+        return render(request,'search.html',{"message":message})
+
 
