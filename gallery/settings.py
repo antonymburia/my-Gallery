f5fa8842faf899284391b752d78797597dcf05bd
antonymburia <antonymburia10@gmail.com>
2022-03-28 10:45:29 +0300

    update files

--- gallery/settings.py
+++ gallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$2!_vg+%5@16#03fyvien92@i6+i7fe(quv*s4+x+)a%%2(2-5'
+SECRET_KEY='$2!_vg+%5@16#03fyvien92@i6+i7fe(quv*s4+x+)a%%2(2-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
